916e5239dcdc0bca7adef1bbadfd7d72a6e100dc
hussainalhabeb1000 <106217548+hussainalhabeb1000@users.noreply.github.com>
2022-05-31 21:29:38 +0300

    Update quizeditor.js

--- quizeditor.js
+++ quizeditor.js
@@ -232,7 +232,7 @@ document.querySelectorAll('iframe')[document.querySelectorAll('iframe').length-1
   }
   else{
   document.querySelectorAll('[id2="imbox"]')[parseInt(document.querySelectorAll('[id="a2b"]')[0].innerText)-1].id=iddd
-  linksaved()
+  
 	  
   }
   
