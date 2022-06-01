1e1f2a770513ed3a0185d1e394858da9bf764010
hussainalhabeb1000 <106217548+hussainalhabeb1000@users.noreply.github.com>
2022-06-01 19:04:48 +0300

    Update quizeditor.js

--- quizeditor.js
+++ quizeditor.js
@@ -754,9 +754,21 @@ function getlen2() {
 idid2=parseInt(id2.values[0][0].slice(3))
 
 
+/////
+document.querySelectorAll('[id="myInput"]')[0].value='https://hussainalhabeb1000.github.io/pages/quizload.html#'+idid2
+  /* Get the text field */
+  var copyText = document.getElementById("myInput");
 
+  /* Select the text field */
+  copyText.select();
+  copyText.setSelectionRange(0, 99999); /* For mobile devices */
 
-
+  /* Copy the text inside the text field */
+  navigator.clipboard.writeText(copyText.value);
+  
+  /* Alert the copied text */
+  /*alert("Copied the text: " + copyText.value);*/
+////
 
     }
   };
