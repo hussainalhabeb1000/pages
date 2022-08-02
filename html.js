4542de28df492d09b0ec91c6661ec678a1809a05
hussainalhabeb1000 <106217548+hussainalhabeb1000@users.noreply.github.com>
2022-08-02 18:55:41 +0300

    Update html.js

--- html.js
+++ html.js
@@ -1,6 +1,6 @@
 
 
-
+alert()
 function windowlocationhash(hashh) { thash=0; while (window.location.hash.slice(2)[thash] != '/') {thash++} thash=thash+4; if (typeof hashh!='undefined'){window.location.hash=window.location.hash.slice(0,-(window.location.hash.slice(thash).length))+hashh}; return decodeURIComponent(window.location.hash.slice(thash)); };
 function deldel() {
 document.body.onload();};document.querySelectorAll('script')[document.querySelectorAll('script').length-1].remove();
