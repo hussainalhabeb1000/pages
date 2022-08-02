9171770feaefe8d5154336a5609808225ed7ffbf
hussainalhabeb1000 <106217548+hussainalhabeb1000@users.noreply.github.com>
2022-08-02 18:44:38 +0300

    Create html.js

---
+++ html.js
@@ -0,0 +1,9 @@
+
+
+
+function windowlocationhash(hashh) { thash=0; while (window.location.hash.slice(2)[thash] != '/') {thash++} thash=thash+4; if (typeof hashh!='undefined'){window.location.hash=window.location.hash.slice(0,-(window.location.hash.slice(thash).length))+hashh}; return decodeURIComponent(window.location.hash.slice(thash)); };
+function deldel() {
+document.body.onload();};document.querySelectorAll('script')[document.querySelectorAll('script').length-1].remove();
+
+
+
